39496354ac44cb39146f0bf4d1a55ecf1173556c
mbaker <mekenna.drabner@gmail.com>
2024-06-22 15:05:08 -0600

    added potential game options with correlating alert boxes, along with play again prompt

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 function game() {
 
-const options = ["rock", "paper", "scissors"];
+  const options = ["rock", "paper", "scissors"];
 
-while (true) {
-    let playerchoice = prompt("Choose rock, paper, or scissors!");
-    if (!options.includes(playerchoice.toLowerCase())) {
-        alert("Invalid choice.  Try again!");
-        continue;
+  while (true) {
+    let playerChoice = prompt("Choose rock, paper, or scissors!");
+    if (!options.includes(playerChoice.toLowerCase())) {
+      alert("Invalid choice.  Try again!");
+      continue;
     }
 
     let computerChoice = options[Math.floor(Math.random() * options.length)];
     alert("Computer chose " + computerChoice + ".");
+
+    if (playerChoice.toLowerCase() === computerChoice) {
+      alert("It's a tie!");
+    } else if (
+      (playerChoice.toLowerCase() === "rock" && computerChoice === "scissors") ||
+      (playerChoice.toLowerCase() === "scissors" && computerChoice === "paper") ||
+      (playerChoice.toLowerCase() === "paper" && computerChoice === "rock")
+    ) {
+      alert("You have won! Congratulations!");
+    } else {
+      alert("You have lost.");
+    }
+
+    if (!confirm("Would you like to play again?")) {
+      break;
+    }
+  }
 }
 
-}
-
-game();
+game(); 
